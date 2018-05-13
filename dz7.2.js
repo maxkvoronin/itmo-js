1bd5a07ce60c9989e093cd79d099d631e8bffe13
maxim <maxkvoronin@gmail.com>
2018-05-13 17:28:21 +0300

    dz7 task 2 done

---
+++ dz7.2.js
@@ -0,0 +1,15 @@
+describe("y", function() {
+
+  it("сумма производной и квадартного корня", function() {
+    assert.equal( y(1), 2);
+  });
+
+  it("при X = -1", function() {
+    assert( isFinite(y(-1)));
+  });
+
+  it("при X = 0", function() {
+    assert( isNaN(y(0)));
+  });
+  
+});
