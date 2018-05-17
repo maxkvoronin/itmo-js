0b715f9aa5a6ac07fda2c588a42d12c4c74f6ae1
maxim <maxkvoronin@gmail.com>
2018-05-17 12:33:53 +0300

    dz8.1 refactor

--- dz8.1.js
+++ dz8.1.js
@@ -6,6 +6,19 @@
 //см. dz8.1.html
 
 var h1 = document.createElement('h1');
+
+var elHrs = document.createElement('span');
+elHrs.style.color = "red";
+h1.appendChild(elHrs);
+
+var elMin = document.createElement('span');
+elMin.style.color = "green";
+h1.appendChild(elMin);
+
+var elSec = document.createElement('span');
+elSec.style.color = "black";
+h1.appendChild(elSec);
+
 document.body.appendChild(h1);
 
 var timerId = setInterval(function getmyTime () {
@@ -19,7 +32,10 @@ var timerId = setInterval(function getmyTime () {
     if (min < 10) min = "0" + min;
     if (sec < 10) sec = "0" + sec;
 
-    h1.innerText = `${hrs}:${min}:${sec}`;
+    elHrs.innerText = hrs + ":";
+    elMin.innerText = min + ":";
+    elSec.innerText = sec;  
+    
 }, 1000);
 
 
