7508b1fad758641ab60980f41b2d5d28828db3f8
maxim <maxkvoronin@gmail.com>
2018-05-19 18:17:16 +0300

    dz9.2 add new event realisations

--- dz9.2.js
+++ dz9.2.js
@@ -7,7 +7,6 @@
  *  увеличиваться на единицу.
 */
 
-
 function buildCount () {
     let count = 0;
         return function inner () { 
@@ -18,13 +17,22 @@ function buildCount () {
 let printToButton1 = buildCount();
 let printToButton2 = buildCount();
 let printToButton3 = buildCount();
+let printToButton4 = buildCount();
 
+//через стрелочную функцию
 elButton1.onclick = (ev) => {
     ev.target.textContent = printToButton1();
 }
 
+//через this 
 elButton2.onclick = function() {
     this.textContent = printToButton2();
 }
 
+//через регистратор обработчика события
 elButton3.addEventListener("click", function() {this.textContent = printToButton3();});
+
+//через переменную CSS
+elButton4.onclick = function() {
+    document.querySelector(':root').style.setProperty("--count", `"${printToButton4()}"`);
+}
