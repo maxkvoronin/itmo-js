ace087cd8234e28b4377f793c685c6b3bbccc84c
maxim <maxkvoronin@gmail.com>
2018-05-24 11:38:09 +0300

    dz10.1 refactor

--- fdz10.1.js
+++ fdz10.1.js
@@ -15,31 +15,42 @@ function Product (pname, price) {
 }
 
 function ProductCart () {
-    var _products = [];
-    var _productsQuantity = 0;
-    var _productsTotalPrice = 0;
-
-    this.add = function (pname, price) {
-        Product.call(this, pname, price);
-        _products[this.pname] = this.price;
-        _productsQuantity++;
-        _productsTotalPrice+=this.price;
+    this._products = [];
+    this._totalCartProducts = 0;
+    this._totalCartPrice = 0;
+   
+    this.add = function (newProduct) {
+        this._products.push({name: newProduct.pname, price: newProduct.price});
     }
 
-    this.getProductsTotalPrice = function () {
-        return  _productsTotalPrice;
+    this.getPriceOfProduct = function (productName) {
+        for (let iProduct of this._products) {
+            if(productName === iProduct.name) 
+                return iProduct.price;
+        }
     }
 
-    this.getProductsQuantity = function () {
-        return  _productsQuantity;
+    this.getTotalCartPrice = function () {
+        for (let iProduct of this._products) {
+            this._totalCartPrice+=iProduct.price;
+        }
+        return  this._totalCartPrice;
+    }
+
+    this.getAmountOfProducts = function () {
+        return  this._totalCartProducts = this._products.length;
     }
     
 }
-//var a = new ProductCart()
-//a.add("keyb1", 1000);
-//a.add("keyb2", 2000);
-//a.add("keyb3", 3000);
 
-//console.log(a.getProductsTotalPrice());
-//console.log(a.getProductsTotalPrice());
-//console.log(a.getProductsQuantity());s
+// var product1 = new Product('клавиатура',1000);
+// var product2 = new Product('монитор',5000);
+// var pCart = new ProductCart();
+
+// pCart.add(product1);
+// pCart.add(product2);
+
+// console.log(pCart._products);
+// console.log(pCart.getPriceOfProduct('монитор'));
+// console.log(pCart.getTotalCartPrice());
+// console.log(pCart.getAmountOfProducts());
